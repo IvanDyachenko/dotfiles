dd181dcdfe60d730a7ab3fdfc4ff5c70a2c56f09
Ivan Dyachenko <vandyachen@gmail.com>
2020-08-21 00:18:49 +0300

    Setting up per-project Metals for Emacs with Nix and Direnv

---
+++ .config/nixpkgs/overlays/metals.nix
@@ -0,0 +1,42 @@
+self: super:
+{
+  metals-emacs =
+    let
+      baseName = "metals-emacs";
+      version = "0.9.3";
+      deps = with super; stdenv.mkDerivation {
+        name = "${baseName}-deps-${version}";
+        buildCommand = ''
+        export COURSIER_CACHE=$(pwd)
+        ${coursier}/bin/coursier fetch org.scalameta:metals_2.12:${version} \
+          -r sonatype:snapshots \
+          -r "bintray:scalacenter/releases" \
+          > deps
+        mkdir -p $out/share/java
+        cp -n $(< deps) $out/share/java/
+        '';
+#       outputHashMode = "recursive";
+#       outputHashAlgo = "sha256";
+#       outputHash     = "???";
+      };
+    in
+      with super; stdenv.mkDerivation rec {
+        name = "${baseName}-${version}";
+#       meta = with stdenv.lib; {
+#         homepage = https://scalameta.org/metals/;
+#         description = "Scala language server with rich IDE features";
+#         license = licenses.asl20;
+#       };
+        doCheck = true;
+        buildInputs = [ jre jdk makeWrapper deps ];
+        phases = [ "installPhase" ];
+        installPhase = ''
+        mkdir -p $out/bin
+        makeWrapper ${jre}/bin/java $out/bin/${baseName} \
+          --prefix PATH : ${lib.makeBinPath [ jdk ]} \
+          --add-flags "-cp $CLASSPATH" \
+          --add-flags "-Xss4m -Xms100m -Dmetals.client=emacs" \
+          --add-flags "scala.meta.metals.Main"
+        '';
+      };
+}
