e85dc7798588c8d886690e1c88bdfa761fdfae2a
Ivan Dyachenko <vandyachen@gmail.com>
2020-11-10 22:41:26 +0300

    Metals v0.9.5

--- .config/nixpkgs/overlays/metals.nix
+++ .config/nixpkgs/overlays/metals.nix
@@ -3,7 +3,7 @@ self: super:
   metals-emacs =
     let
       baseName = "metals-emacs";
-      version = "0.9.4";
+      version = "0.9.5";
       deps = with super; stdenv.mkDerivation {
         name = "${baseName}-deps-${version}";
         buildCommand = ''
