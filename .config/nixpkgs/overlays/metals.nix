2bc28378427ea51e9b1099cd9f6e46bac6147020
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-04-07 20:00:27 +0300

    Metals v0.10.1 - Tungsten Update org.scalameta:metals_2.12 to 0.10.1 https://scalameta.org/metals/blog/2021/04/06/tungsten.html

--- .config/nixpkgs/overlays/metals.nix
+++ .config/nixpkgs/overlays/metals.nix
@@ -3,7 +3,7 @@ self: super:
   metals-emacs =
     let
       baseName = "metals-emacs";
-      version = "0.10.0";
+      version = "0.10.1";
       deps = with super; stdenv.mkDerivation {
         name = "${baseName}-deps-${version}";
         buildCommand = ''
