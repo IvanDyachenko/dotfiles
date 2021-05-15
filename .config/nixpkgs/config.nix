de5cdce09c00dd1f0f53d4a8eafa0505501df0ee
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-05-16 02:49:21 +0300

    Update `.gitignore`

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -4,6 +4,7 @@
   environment = pkgs: with pkgs; {
     systemPackages = [
       direnv # augments existing shells with a new feature that can load and unload environment variables depending on the current directory.
+      emacs
     ];
   };
 }
