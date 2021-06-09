ea6c756bb9e3af93bd5dc8f9eff22dcc3e9e1f56
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-06-09 22:52:02 +0300

    Improve description of the used system packages

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -3,8 +3,13 @@
 
   environment = pkgs: with pkgs; {
     systemPackages = [
-      direnv # augments existing shells with a new feature that can load and unload environment variables depending on the current directory.
-      emacs
+      # Augments existing shells with a new feature that can load and unload
+      # environment variables depending on the current directory.
+      direnv
+
+      # Emacs that is built from the latest tag.
+      # https://github.com/nix-community/emacs-overlay
+      emacsUnstable
     ];
   };
 }
