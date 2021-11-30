f7b1f4af4a6b0eae36ddb061fc4dc85ae06aa8f7
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-11-30 20:54:46 +0300

    Declarative system configurations using `flake`, `nix-darwin`, and `home-manager`

---
+++ .config/nixpkgs/darwin-configuration.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, home-manager, ... }:
+
+{
+  imports = [ home-manager.darwinModules.home-manager ];
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  # Install the flakes edition.
+  nix.package = pkgs.nixFlakes;
+
+  # Enable the nix 2.0 CLI and flakes support feature-flags.
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  # You won't be able to install or search for an unfree package as a user,
+  # unless you explicitly enable it:
+  nixpkgs.config.allowUnfree = true;
+
+  nixpkgs.overlays = [
+    (import ./overlays/emacs.nix)
+    (import ./overlays/metals.nix)
+    (import ./overlays/scala-cli.nix)
+  ];
+
+  users.users.ivandyachenko = {
+    name = "Ivan";
+    home = "/Users/ivandyachenko";
+  };
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  # environment.systemPackages = [ ];
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.fish.enable = true;
+  environment.variables.SHELL = "${pkgs.fish}/bin/fish";
+
+  # By default, Home Manager uses a private pkgs instance that is configured via
+  # the home-manager.users.<name>.nixpkgs options. To instead use the global pkgs
+  # that is configured via the system level nixpkgs options, set:
+  home-manager.useGlobalPkgs = true;
+
+  # By default packages will be installed to $HOME/.nix-profile but they can be
+  # installed to /etc/profiles by setting:
+  home-manager.useUserPackages = true;
+
+  home-manager.users.ivandyachenko = import ./home.nix;
+}
---
+++ .config/nixpkgs/home.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.05";
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  # home.username = "ivandyachenko";
+  # home.homeDirectory = "/Users/ivandyachenko";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    direnv # Load and unload environment variables depending on the current directory.
+    nixfmt # A formatter for Nix code.
+    treefmt # One CLI to format the code tree.
+    metals # Scala language server with rich IDE features.
+    scala-cli # Scala CLI is an experimental tool to run/compile/test Scala that aims at being a better scala command.
+  ];
+
+  programs.emacs.enable = true;
+}
