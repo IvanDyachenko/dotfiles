a236a2875de13cfe55ccfce65a5154f71c50355b
Ivan Dyachenko <vandyachen@gmail.com>
2023-09-20 14:12:34 +0300

    Update dependencies

--- .config/nixpkgs/overlays/emacs.nix
+++ .config/nixpkgs/overlays/emacs.nix
@@ -1,4 +1,4 @@
 (import (builtins.fetchTarball {
   url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-  sha256 = "0bqw75xf5slvf5w6z16z4b0pdg5j7azf26jijjnzb0gvznxdkn02";
+  sha256 = "0b0q2m97ms9qma6y62902acpjkx5cd4pz2lvxgrzrk6fyax7cjrf";
 }))
