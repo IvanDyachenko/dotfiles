b2720fd5753063df314a1e95e74019491d036387
Ivan Dyachenko <inbox@ivandyachenko.ru>
2024-09-02 17:48:18 +0300

    treefmt

--- .config/nixpkgs/overlays/neovim.nix
+++ .config/nixpkgs/overlays/neovim.nix
@@ -1,4 +1,5 @@
 (import (builtins.fetchTarball {
-  url = "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
+  url =
+    "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
   sha256 = "0xp4hm5hjg1vpkjz9p3i1j13jd71snkw270gi3jwwbcid86z398a";
 }))
