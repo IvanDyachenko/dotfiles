d373758fbe84e116b37db0c9895084bd05a9d69e
Ivan Dyachenko <inbox@ivandyachenko.ru>
2024-09-29 12:03:33 +0300

    `neovim` configuration

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,28 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  
+  build = ':TSUpdate',
+  
+  config = function () 
+    require('nvim-treesitter.configs').setup({
+      indent = {
+        enable = true
+      },
+      highlight = {
+        enable = true
+      },
+      treesitter = {
+        enable = false
+      },
+      playground = {
+        enable = true
+      },
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+      ensure_installed = "all"
+    })
+  end
+ }
