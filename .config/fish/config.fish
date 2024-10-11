06a252d5fd60672c83ea1a4b37bc3251c80e2455
Ivan Dyachenko <inbox@ivandyachenko.ru>
2024-10-11 14:13:29 +0300

    Update `fish`'s configuration files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,24 @@
+# Load "Dracula" theme.
+fish_config theme choose "Dracula Official"
+
+# Run foreign bash scripts and capture exported environment variables.
+set fish_function_path $fish_function_path $HOME/.config/fish/plugins/plugin-foreign-env/functions
+
+# direnv is an extension for your shell.
+# It augments existing shells with a new feature that can load and unload environment variables 
+# depending on the current directory.
+eval (direnv hook fish)
+
 # Configure pinentry to use the correct TTY.
 # https://wiki.archlinux.org/index.php/GnuPG#Configure_pinentry_to_use_the_correct_TT
 set -x GPG_TTY (tty)
 
-set -gx HOMEBREW_PREFIX "/opt/homebrew"
-set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar"
-set -gx HOMEBREW_REPOSITORY "/opt/homebrew"
-fish_add_path -gP "/opt/homebrew/bin" "/opt/homebrew/sbin"
-! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH
-! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH
-
-# Add fenv to path
-set fish_function_path $fish_function_path $HOME/.config/fish/plugins/plugin-foreign-env/functions
-
-# Source Nix setup script
-# fenv source ~/.nix-profile/etc/profile.d/nix.sh
-
-# For direnv to work properly it needs to be hooked into the shell.
-eval (direnv hook fish)
+# Load homebrew.
+if test -d /opt/homebrew
+	set -gx HOMEBREW_PREFIX "/opt/homebrew"
+	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
+end
+! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH
+! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH
+fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin"
